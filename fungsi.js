f880c3eecd9d8a894b357d3d9b321667d1068e01
R. Muhamad <anakebakulgesek1@gmail.com>
2023-04-11 13:35:45 +0700

    revisi comit kali

--- fungsi.js
+++ fungsi.js
@@ -1,5 +1,8 @@
+console.log('fungsi pertama');
+
 const  add = (a,c) => a + c;
 console.log(add(1,3));
+<<<<<<< HEAD
 
 const subtract = (a, b) => a - b;
 console.log(subtract(5,2));
@@ -9,3 +12,7 @@ console.log(multiply(2, 3));
 
 const divide = (a, b) => a / b;
 console.log(divide(8,4));
+=======
+const subtract = (a, b) => a + b;
+console.log(subtract(5,2));
+>>>>>>> parent of 6c47176 (tambah fitur kali)
