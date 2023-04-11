6c47176a2da8f8d923d1f24161e57020a08ebcae
R. Muhamad <anakebakulgesek1@gmail.com>
2023-04-11 13:16:54 +0700

    tambah fitur kali

--- fungsi.js
+++ fungsi.js
@@ -1,6 +1,8 @@
-console.log('fungsi pertama');
-
 const  add = (a,c) => a + c;
 console.log(add(1,3));
-const subtract = (a, b) => a + b;
+
+const subtract = (a, b) => a - b;
 console.log(subtract(5,2));
+
+const multiply = (a, b) => a * b;
+console.log(multiply(2, 3));
