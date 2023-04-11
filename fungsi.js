ba4a97fd8ca42cafdbdbbbcaa427a33b6451dd7a
R. Muhamad <anakebakulgesek1@gmail.com>
2023-04-11 22:24:44 +0700

    tambah MathSin

--- fungsi.js
+++ fungsi.js
@@ -2,17 +2,15 @@ console.log('fungsi pertama');
 
 const  add = (a,c) => a + c;
 console.log(add(1,3));
-<<<<<<< HEAD
 
 const subtract = (a, b) => a - b;
 console.log(subtract(5,2));
 
-const multiply = (a, b) => a * b;
-console.log(multiply(2, 3));
+const multiplyNumber = (a, b) => a * b;
+console.log(multiplyNumber(2, 3));
 
 const divide = (a, b) => a / b;
 console.log(divide(8,4));
-=======
-const subtract = (a, b) => a + b;
-console.log(subtract(5,2));
->>>>>>> parent of 6c47176 (tambah fitur kali)
+
+const mathSin = (number) => Math.sin(number);
+console.log(mathSin(10));
